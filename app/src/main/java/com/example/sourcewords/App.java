6d21152d1a828d4503bcb47a8e38fd2e15984b85
xiaozhu-sorce <790362227@qq.com>
2022-01-23 10:11:47 +0800

    add: mine_xml

--- app/src/main/java/com/example/sourcewords/App.java
+++ app/src/main/java/com/example/sourcewords/App.java
@@ -11,6 +11,11 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         context = getAppContext();
+//
+//        //设置状态栏透明
+//        makeStatusBarTransparent(this);
+//        //状态栏文字自适应
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 
 
