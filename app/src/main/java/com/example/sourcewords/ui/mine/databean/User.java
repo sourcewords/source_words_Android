1636dad8ba15e554a1a6d7d91b269079f1f9380b
刘昱欣 <2590177089@qq.com>
2022-01-30 19:25:23 +0800

    user

---
+++ app/src/main/java/com/example/sourcewords/ui/mine/databean/User.java
@@ -0,0 +1,22 @@
+package com.example.sourcewords.ui.mine.databean;
+
+public class User {
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String password;
+    private String name;
+}
---
+++ app/src/main/java/com/example/sourcewords/ui/mine/databean/UserInfo.java
@@ -0,0 +1,67 @@
+package com.example.sourcewords.ui.mine.databean;
+
+public class UserInfo {
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    private String phone;
+    private String signature;
+    private String email;
+    private Integer gender;
+    private String location;
+    private String name;
+    private String birthDay;
+}
