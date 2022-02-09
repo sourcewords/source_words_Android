8e53c40b70fae1764c3f854a66c4c68457de3b67
tiddster <864187149@qq.com>
2022-02-09 12:07:46 +0800

    database

--- app/src/main/java/com/example/sourcewords/ui/review/model/WordRepository.java
+++ app/src/main/java/com/example/sourcewords/ui/review/model/WordRepository.java
@@ -27,9 +27,23 @@ public class WordRepository {
         dao = db.getWordDao();
     }
 
+    public void Insert(WordRoot...wordRoots){
+        new Insert(dao).execute(wordRoots);
+    }
+
+    public void Delete(WordRoot...wordRoots){
+        new Delete(dao).execute(wordRoots);
+    }
+
+    public void Update(WordRoot...wordRoots){
+        new Update(dao).execute(wordRoots);
+    }
+
     public LiveData<WordRoot> getWordRoot(int id) {
         return dao.getWordRoot(id);
     }
+
+    public LiveData<List<WordRoot>> getAllWordRoot(){return  dao.getAllWordRoot();}
 
     public WordRoot getWordRootTest(int id) {
         List<Word> list = new ArrayList<>();
@@ -44,5 +58,44 @@ public class WordRepository {
 
     public WordRoot getWordRootTest2(int id) {
         return null;
+    }
+
+    static class Insert extends AsyncTask<WordRoot,Void,Void>{
+        WordRootDao wordDao;
+        Insert(WordRootDao wordDao){
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(WordRoot... wordRoots) {
+            wordDao.insertRoot(wordRoots);
+            return null;
+        }
+    }
+
+    static class Delete extends AsyncTask<WordRoot,Void,Void>{
+        WordRootDao wordDao;
+        Delete(WordRootDao wordDao){
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(WordRoot... wordRoots) {
+            wordDao.deleteRoot(wordRoots);
+            return null;
+        }
+    }
+
+    static class Update extends AsyncTask<WordRoot,Void,Void>{
+        WordRootDao wordDao;
+        Update(WordRootDao wordDao){
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(WordRoot... wordRoots) {
+            wordDao.updateRoot(wordRoots);
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/example/sourcewords/ui/review/viewmodel/ReviewViewModel.java
@@ -0,0 +1,29 @@
+package com.example.sourcewords.ui.review.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.sourcewords.ui.review.dataBean.WordRoot;
+import com.example.sourcewords.ui.review.model.WordRepository;
+
+public class ReviewViewModel extends AndroidViewModel {
+    private WordRepository mWordRepository;
+    public ReviewViewModel(@NonNull Application application) {
+        super(application);
+        mWordRepository = new WordRepository();
+    }
+
+    public void Insert(WordRoot...wordRoots){
+        mWordRepository.Insert(wordRoots);
+    }
+
+    public void Delete(WordRoot...wordRoots){
+        mWordRepository.Delete(wordRoots);
+    }
+
+    public void Update(WordRoot...wordRoots){
+        mWordRepository.Update(wordRoots);
+    }
+}
