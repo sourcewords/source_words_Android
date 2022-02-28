7382c0049cc5887898c18ad1d9bd619a95f6aa1c
刘昱欣 <2590177089@qq.com>
2022-02-28 18:40:49 +0800

    1

--- app/src/main/java/com/example/sourcewords/ui/login/view/LoginActivity.java
+++ app/src/main/java/com/example/sourcewords/ui/login/view/LoginActivity.java
@@ -75,7 +75,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
         binding.registerButton.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
