7e72b6b9b68ff8589f100287162ea6f77cb48eff
xiaozhu-sorce <790362227@qq.com>
2022-03-19 14:24:28 +0800

    ui_back

--- app/src/main/java/com/example/sourcewords/ui/mine/view/SettingActivity.java
+++ app/src/main/java/com/example/sourcewords/ui/mine/view/SettingActivity.java
@@ -2,6 +2,7 @@ package com.example.sourcewords.ui.mine.view;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,17 @@ import com.example.sourcewords.R;
 
 public class SettingActivity  extends AppCompatActivity {
 
+    private ImageButton back;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        back = findViewById(R.id.setting_back);
+
+        back.setOnClickListener(v ->
+                finish());
 
         getWindow().getDecorView().
                 setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
--- app/src/main/java/com/example/sourcewords/ui/mine/view/UserAgreementActivity.java
+++ app/src/main/java/com/example/sourcewords/ui/mine/view/UserAgreementActivity.java
@@ -2,6 +2,7 @@ package com.example.sourcewords.ui.mine.view;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,17 @@ import com.example.sourcewords.R;
 
 public class UserAgreementActivity extends AppCompatActivity {
 
+    private ImageButton back;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_useragreement);
+
+        back = findViewById(R.id.user_agreement_back);
+
+        back.setOnClickListener(v ->
+                finish());
 
         getWindow().getDecorView().
                 setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
